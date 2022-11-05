aacf5fbe0257cf0c2aec334baaf575b9b672fe4a
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-11-05 17:43:49 -0400

    adding img is done

--- public/js/storeapi.js
+++ public/js/storeapi.js
@@ -4,9 +4,16 @@ var submitButtonEl = document.querySelector('#btnSubmit');
 var search_resultsEl=document.querySelector('#fullResults')
 
 submitButtonEl.addEventListener('click',()=>{
-    target_API_Endpoint();
-})
+
+   //target_API_Endpoint();
+
+ stud();
+
+
+  
+});
 var tartarget_API_Endpoint= [];
+var walmart_API_Endpoint= [];
 function target_API_Endpoint(){
 
     // // set up the request parameters
@@ -17,35 +24,103 @@ function target_API_Endpoint(){
     //   'rating': 'four_star'
     // }
     // console.log(params);
-
-    var queryItem = `https://api.redcircleapi.com/request?api_key=${Target_Token}&search_term=xbox&type=search&output=json`;
+  var queryItem = `https://api.redcircleapi.com/request?api_key=${Target_Token}&search_term=xbox&type=search&output=json`;
   //  var queryItem = `https://api.redcircleapi.com/request?${params}`;
-    fetch(queryItem, { method: 'GET' }) //fetching all related area for current location
-    .then((response) => response.json()).then((data) => {
+  fetch(queryItem, { method: 'GET' }) //fetching all related area for current location
+  .then((response) => response.json()).then((data) => {
         var i =0
        do{
         var target_search_Results={
-            product_img: JSON.stringify(data.search_results[i].product['main_image']),
+            // product_img: JSON.stringify(data.search_results[i].product['main_image']),
+            product_img: "https://hackaday.com/wp-content/uploads/2011/08/temporary-image.gif",
             title: JSON.stringify(data.search_results[i].product['title']),
             product_Link: JSON.stringify(data.search_results[i].product['link']),
             primary_price: JSON.stringify(data.search_results[i].offers.primary['symbol']+' '+data.search_results[i].offers.primary['price']),
             sale_price: JSON.stringify(data.search_results[i].offers.primary['symbol']+' '+data.search_results[i].offers.primary['price']),
-          
-            
-
         }
-        tartarget_API_Endpoint.push(target_search_Results)
-
-         
-
+        tartarget_API_Endpoint.push(target_search_Results);
          i++;
        }
        while(i<data.search_results.length)
        console.log(tartarget_API_Endpoint)
+       cratecard();
 
     })
-    .catch((error) => {
+  .catch((error) => {
       console.error('Error:', error);
       return error;
     });
+  
 }
+
+function  cratecard(){
+
+  for (var i = 0; i < tartarget_API_Endpoint.length; i++) {
+    var addDivEl = document.createElement('div');
+    addDivEl.classList.add('card');
+    var imgEl = document.createElement('img')
+    imgEl.classList.add('card-img-top');
+    imgEl.src=`${tartarget_API_Endpoint[i].product_img}`;;
+  
+    addDivEl.appendChild(imgEl)
+    search_resultsEl.appendChild(addDivEl);
+  }
+}
+
+function stud(){
+  var TestDummytartarget_API_Endpoint={
+              product_img: [
+                "https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-9.jpg?itok=AZn0PDgk", 
+                "https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-9.jpg?itok=AZn0PDgk", 
+                "https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-9.jpg?itok=AZn0PDgk","https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-9.jpg?itok=AZn0PDgk",
+                 "https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-9.jpg?itok=AZn0PDgk", "https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-9.jpg?itok=AZn0PDgk",
+              ],
+              title:["1.Kabir", "2.Faisal", "3.WOrld", "4.Hello"], 
+              product_Link: [
+                "www.google.com","www.google.com","www.google.com","www.google.com"
+              ],
+              primary_price: ["11.00","12.00","13.00","14.00"],
+              sale_price: ["1.00","2.00","3.00","4.00"]
+             }
+  for (var i = 0; i < TestDummytartarget_API_Endpoint.length; i++) {
+    var addDivEl = document.createElement('div');
+    addDivEl.classList.add('card');
+    var imgEl = document.createElement('img')
+    imgEl.classList.add('card-img-top');
+    imgEl.src=`${TestDummytartarget_API_Endpoint[i]}`;;
+  
+    addDivEl.appendChild(imgEl)
+    search_resultsEl.appendChild(addDivEl);
+    console.log(search_resultsEl);
+
+
+  }
+}
+
+// function walmart_API_Endpoint(){
+
+// var queryItem = `https://api.redcircleapi.com/request?api_key=${Target_Token}&search_term=xbox&type=search&output=json`;
+// //  var queryItem = `https://api.redcircleapi.com/request?${params}`;
+// fetch(queryItem, { method: 'GET' }) //fetching all related area for current location
+// .then((response) => response.json()).then((data) => {
+//       var i =0
+//      do{
+//       var target_search_Results={
+//           product_img: JSON.stringify(data.search_results[i].product['main_image']),
+//           title: JSON.stringify(data.search_results[i].product['title']),
+//           product_Link: JSON.stringify(data.search_results[i].product['link']),
+//           primary_price: JSON.stringify(data.search_results[i].offers.primary['symbol']+' '+data.search_results[i].offers.primary['price']),
+//           sale_price: JSON.stringify(data.search_results[i].offers.primary['symbol']+' '+data.search_results[i].offers.primary['price']),
+//       }
+//       tartarget_API_Endpoint.push(target_search_Results);
+//        i++;
+//      }
+//      while(i<data.search_results.length)
+//      console.log(tartarget_API_Endpoint)
+
+//   })
+// .catch((error) => {
+//     console.error('Error:', error);
+//     return error;
+//   });
+// }
