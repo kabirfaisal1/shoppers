c9c4ea716fa7232b229cc9b1c31e161e32f3407b
amiller0806 <ariel.miller555@gmail.com>
2022-11-05 18:45:14 -0400

    resolve merge conflict

--- public/js/storeapi.js
+++ public/js/storeapi.js
@@ -1,22 +1,51 @@
-var submitButton = document.querySelector("#btnSubmit");
+var Target_Token = '4324A15C75444BB7AA69766FA38667EF'
+var searchInputEL = document.querySelector("search-fild");
+var submitButtonEl = document.querySelector('#btnSubmit');
+var search_resultsEl=document.querySelector('#fullResults')
 
-submitButton.addEventListener("click",()=>{
-    getItem();
+submitButtonEl.addEventListener('click',()=>{
+    target_API_Endpoint();
 })
-function getItem(){
-    var searchesults=document.querySelector('#search_results')
-    var queryItem = `https://api.redcircleapi.com/request?api_key=4324A15C75444BB7AA69766FA38667EF&search_term=xbox&type=search&output=json`;
+var tartarget_API_Endpoint= [];
+function target_API_Endpoint(){
+
+    // // set up the request parameters
+    // const params = {
+    //   'api_key': Target_Token,
+    //   'search_term': 'xbox',
+    //   'type': 'search',
+    //   'rating': 'four_star'
+    // }
+    // console.log(params);
+
+    var queryItem = `https://api.redcircleapi.com/request?api_key=${Target_Token}&search_term=xbox&type=search&output=json`;
+  //  var queryItem = `https://api.redcircleapi.com/request?${params}`;
     fetch(queryItem, { method: 'GET' }) //fetching all related area for current location
     .then((response) => response.json()).then((data) => {
-        for(var i=0; i<data.search_results.length; i++){
-            console.log(JSON.stringify(data.search_results[i].product["title"]))
+        var i =0
+       do{
+        var target_search_Results={
+            product_img: JSON.stringify(data.search_results[i].product['main_image']),
+            title: JSON.stringify(data.search_results[i].product['title']),
+            product_Link: JSON.stringify(data.search_results[i].product['link']),
+            primary_price: JSON.stringify(data.search_results[i].offers.primary['symbol']+' '+data.search_results[i].offers.primary['price']),
+            sale_price: JSON.stringify(data.search_results[i].offers.primary['symbol']+' '+data.search_results[i].offers.primary['price']),
+          
+            
+
         }
+        tartarget_API_Endpoint.push(target_search_Results)
+
+         
+
+         i++;
+       }
+       while(i<data.search_results.length)
+       console.log(tartarget_API_Endpoint)
 
     })
     .catch((error) => {
       console.error('Error:', error);
       return error;
     });
-
-
 }
