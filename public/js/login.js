94faa923418d9f1914179ef85e08e2b4626eba56
amiller0806 <ariel.miller555@gmail.com>
2022-11-05 17:37:58 -0400

    fetch request for user data in login.js

---
+++ public/js/login.js
@@ -0,0 +1,60 @@
+// Make sure everything matches our code, modify this code according to ours 
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+  // MAKE fetch call to is the one in user-routes with the '/login' endpoint.
+//create user login: /api/users/login
+//EXPECTS FROM BODY: {email: 'userEmail@email.com', password: 'usersPassword'}
+// router.post('/login', (req,res) => {
--- public/js/storeapi.js
+++ public/js/storeapi.js
@@ -4,7 +4,7 @@ submitButton.addEventListener("click",()=>{
     getItem();
 })
 function getItem(){
-    var searchresults=document.querySelector('#search_results')
+    var searchesults=document.querySelector('#search_results')
     var queryItem = `https://api.redcircleapi.com/request?api_key=4324A15C75444BB7AA69766FA38667EF&search_term=xbox&type=search&output=json`;
     fetch(queryItem, { method: 'GET' }) //fetching all related area for current location
     .then((response) => response.json()).then((data) => {
