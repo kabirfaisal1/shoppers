76272b5ee461742abdb22eaf56f0646dee2f8c40
cpkopp <cameronpkopp@gmail.com>
2022-11-06 01:00:18 -1000

    all info on logged in user now sent back to homepage, including past searches

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,10 +5,33 @@ const { User, Search } = require('../models');
 
 //HOME ROUTES ===============================================
 router.get('/', (req, res) => {
-    console.log('****************');
-    console.log(req.session);
-    console.log('****************');
-    res.render('homepage', {loggedIn: req.session.loggedIn});
+    if (!req.session.loggedIn) {
+        res.render('homepage', { loggedIn: false });
+    } else {
+        User.findOne({
+            where: { id: req.session.user_id },
+            attributes: ['id','username','email'],
+            include: {
+                model: Search,
+                attributes: ['id','product_name']
+            }
+        })
+            .then(dbData => {
+                const loggedInUser = dbData.get({ plain: true });
+                console.log('==================');
+                console.log(loggedInUser);
+                console.log('==================');
+                res.render('homepage', { 
+                    loggedInUser,
+                    loggedIn: req.session.loggedIn   
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+    
 });
 
 router.get('/login', (req, res) =>{
