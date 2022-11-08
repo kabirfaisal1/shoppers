a5d1db90d7cbe4b83bcdb53760e6ef772a28abb5
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-11-08 16:38:17 -0500

    added cypress test

---
+++ app_Test/cypress/tests/loginTests.cy.js
@@ -0,0 +1,16 @@
+describe('Test to login', () => {
+  it('Should be able to submit a successful submission via contact us form', () => {
+    cy.visit('');
+    //cy.get('#contact-us').click({force: true})
+    //email
+    cy.get('[id="<TO DO>"]').type('');
+    //passed
+    cy.get('[id="<TO DO>"]').type('');
+    //click on log in
+    cy.get('[id="<TO DO>"]').click();
+  });
+
+  it('', () => {
+    //cypress code
+  });
+});
