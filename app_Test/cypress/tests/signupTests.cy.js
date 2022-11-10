225a8acdf2e2fe8cb7ac5e1251adc9bfa9a33022
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-11-09 20:05:15 -0500

    added sing up test and added id in sign up

--- app_Test/cypress/tests/signupTests.cy.js
+++ app_Test/cypress/tests/signupTests.cy.js
@@ -1,28 +1,30 @@
 describe('Test Sign up Page', () => {
     it('Should be able to submit a successful sign up form', () => {
       cy.visit('http://localhost:3001/login');
-      //cy.get('#contact-us').click({force: true})
+      //name
+      cy.get('[id="username"]').should('be.visible').type('Cy Tester');
       //email
-      cy.get('[id="current-email"]').should('be.visible').type('testing12@gmail.com');
-      //passed
-      cy.get('[id="current-password"]').should('be.visible').type('testing123');
+      cy.get('[id="email"]').should('be.visible').type(`testing${Math.random()*2}@gmail.com`);
+      //password
+      cy.get('[id="password"]').should('be.visible').type('testing123');
       //click on log in
-      cy.get('[id="submit"]').should('be.visible').click();
+      cy.get('[id="sign-up"]').should('be.visible').click();
       //verify you are logged in 
       cy.get('[id=logout]').should('be.visible')
     });
   
     it('Should be able to verify log in due duplicate email', () => {
       cy.visit('http://localhost:3001/login');
-      //cy.get('#contact-us').click({force: true})
-      //email
-      cy.get('[id="current-email"]').should('be.visible').type('testing12@gmail.com');
-      //passed
-      cy.get('[id="current-password"]').should('be.visible').type('testing');
-      //click on log in
-      cy.get('[id="submit"]').should('be.visible').click();
+       //name
+       cy.get('[id="username"]').should('be.visible').type('Cy Tester');
+       //email
+       cy.get('[id="email"]').should('be.visible').type('testing123@gmail.com');
+       //password
+      cy.get('[id="password"]').should('be.visible').type('testing');
+      //click on submit
+      cy.get('[id="sign-up"]').should('be.visible').click();
       //verify you are logged in 
-      cy.get('[id=login-error]').should('be.visible').invoke('text').should('contains',"We're sorry, there is an error with your email and/or password.")
+      cy.get('[id=signup-error]').should('be.visible').invoke('text').should('include',"Email address already in use")
     });
   
   });
